7f544affae94403d482b68a3164688e1dc175c85
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-12-07 12:37:13 +0200

    Add day 1
    To run the app specify the input file - it's in the project folder.

---
+++ AocDay1/AocDay1/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.IO
+
+let flip f = fun a b -> f b a
+
+let positive input = if input > 0 then Some input else None
+
+let pair a = (a, a)
+
+let getFuel = flip (/) 3 >> flip (-) 2 >> int
+
+let getTotalFuel = Seq.unfold (getFuel >> positive >> Option.map pair) >> Seq.sum
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length < 1 then
+        printfn "The input file hasn't been specified"
+    else
+        argv.[0]
+        |> File.ReadLines
+        |> Seq.map int
+        |> Seq.sumBy getTotalFuel
+        |> printfn "%i"
+    0
