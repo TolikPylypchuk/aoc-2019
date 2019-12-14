c1d36815d8f4bafb8831a8f9fbc3cb1c76f4d603
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2019-12-14 16:24:30 +0200

    Add day 7

---
+++ AocDay7/Domain.fs
@@ -0,0 +1,117 @@
+﻿namespace AocDay7
+
+open System
+
+type ParameterMode = Position | Immediate
+
+type Operation = Add | Multiply | Read | Write | JumpIfTrue | JumpIfFalse | LessThan | Equals | Halt
+
+type Command = {
+    Operation : Operation
+    ParameterModes : ParameterMode list
+}
+
+type CompleteResult = {
+    Program : int []
+    Outputs : int list
+}
+
+type IncompleteResult = {
+    Program : int []
+    Outputs : int list
+    CurrentPosition : int
+}
+
+type ProgramResult = Complete of CompleteResult | Incomplete of IncompleteResult
+
+[<AutoOpen>]
+module Util =
+    
+    let stringToInt (str : string) =
+        let (success, value) = Int32.TryParse str
+        if success then Some value else None
+    
+    let split (ch : char) (str : string) = str.Split(ch)
+
+    let digitAt position num = num % (pown 10 position) / (pown 10 (position - 1))
+
+    let withoutLast list = list |> List.rev |> List.tail |> List.rev
+
+    let exactlyTwo list =
+        match list with
+        | [x; y] -> Some (x, y)
+        | _ -> None
+
+    let distrib e list =
+        let rec distrib' pre post = seq {
+            match post with
+            | [] -> yield list @ [e]
+            | head :: tail ->
+                yield List.rev pre @ [e] @ post
+                yield! distrib' (head :: pre) tail 
+        }
+
+        distrib' [] list
+        
+    let rec perms list =
+        match list with
+        | [] -> Seq.singleton []
+        | head :: tail -> Seq.collect (distrib head) (perms tail)
+
+    let tryTail = function _ :: tail -> Some tail | [] -> None
+
+module ParameterMode =
+
+    let ofInt param =
+        match param with
+        | 0 -> Some Position
+        | 1 -> Some Immediate
+        | _ -> None
+
+module Operation =
+
+    let ofInt code =
+        match code with
+        | 1 -> Some Add
+        | 2 -> Some Multiply
+        | 3 -> Some Read
+        | 4 -> Some Write
+        | 5 -> Some JumpIfTrue
+        | 6 -> Some JumpIfFalse
+        | 7 -> Some LessThan
+        | 8 -> Some Equals
+        | 99 -> Some Halt
+        | _ -> None
+
+    let numParameters operation =
+        match operation with
+        | Add | Multiply | LessThan | Equals -> 3
+        | JumpIfTrue | JumpIfFalse -> 2
+        | Read | Write -> 1
+        | Halt -> 0
+
+module Command =
+
+    let ofInt command = maybe {
+        let! operation = command % 100 |> Operation.ofInt
+        let numParameters = operation |> Operation.numParameters
+        
+        let! parameterModes =
+            [ 0 .. numParameters - 1 ]
+            |> List.map (fun i -> command |> digitAt (i + 3))
+            |> List.map ParameterMode.ofInt
+            |> sequenceOption
+            
+        return {
+            Operation = operation
+            ParameterModes = parameterModes
+        }
+    }
+
+module ProgramResult =
+
+    let outputs result =
+        match result with
+        | Incomplete result -> result.Outputs
+        | Complete result -> result.Outputs
+    
---
+++ AocDay7/Program.fs
@@ -0,0 +1,124 @@
+﻿module AocDay7.Program
+
+open System.IO
+
+let rec execute' inputs outputs index (nums : int []) = maybe {
+    let! command = Command.ofInt nums.[index]
+    let numParams = command.Operation |> Operation.numParameters
+
+    if index < (nums |> Array.length) - numParams then
+        let parameters =
+            if numParams > 0 then
+                [ index + 1 .. index + numParams - 1 ]
+                |> List.map (fun i -> nums.[i])
+                |> List.zip (command.ParameterModes |> withoutLast)
+                |> List.map (fun (mode, num) -> match mode with Position -> nums.[num] | Immediate -> num)
+            else
+                []
+            
+        let location = nums.[index + numParams]
+
+        let continue' inputs outputs = execute' inputs outputs (index + numParams + 1)
+
+        let replaceAndContinue inputs outputs result =
+            nums
+            |> Array.mapi (fun i num -> if i = location then result else num)
+            |> continue' inputs outputs
+
+        let defaultReplaceAndContinue result = replaceAndContinue inputs outputs result
+
+        let jump predicate = maybe {
+            let! mode = command.ParameterModes |> List.rev |> List.tryHead
+
+            let location =
+                if parameters |> List.forall predicate then
+                    let value = nums.[index + numParams]
+                    match mode with
+                    | Position -> nums.[value]
+                    | Immediate -> value
+                else
+                    index + numParams + 1
+
+            return! nums |> execute' inputs outputs location
+        }
+
+        match command.Operation with
+        | Add ->
+            return! parameters |> List.fold (+) 0 |> defaultReplaceAndContinue
+        | Multiply ->
+            return! parameters |> List.fold (*) 1 |> defaultReplaceAndContinue
+        | Read ->
+            return!
+                match inputs with
+                | input :: otherInputs -> input |> replaceAndContinue otherInputs outputs
+                | [] -> Incomplete { Program = nums; Outputs = outputs; CurrentPosition = index } |> Some
+        | Write ->
+            let! mode = command.ParameterModes |> List.tryHead
+            let result = match mode with Position -> nums.[location] | Immediate -> location
+            return! nums |> continue' inputs (result :: outputs)
+        | JumpIfTrue -> return! jump <| (<>) 0
+        | JumpIfFalse -> return! jump <| (=) 0
+        | LessThan ->
+            let! a, b = parameters |> exactlyTwo
+            return! (if a < b then 1 else 0) |> defaultReplaceAndContinue
+        | Equals ->
+            let! a, b = parameters |> exactlyTwo
+            return! (if a = b then 1 else 0) |> defaultReplaceAndContinue
+        | Halt ->
+            return Complete { Program = nums; Outputs = outputs }
+}
+
+let execute inputs nums = execute' inputs [] 0 nums
+
+let continue' inputs result = execute' inputs [] result.CurrentPosition result.Program
+
+let plug previousResult currentResult = maybe {
+    let! previousResult = previousResult
+    return!
+        match currentResult with
+        | Complete result -> result |> Complete |> Some
+        | Incomplete result -> continue' (previousResult |> ProgramResult.outputs) result
+}
+
+let amplify firstResult = List.scan plug (Some firstResult) >> sequenceOption >> Option.bind tryTail
+
+let amplifyAll results = maybe {
+    let firstResult = Complete { Program = Array.empty; Outputs = [0] }
+
+    let! results = amplify firstResult results
+
+    let rec continueAmplifying results = maybe {
+        let! lastResult = results |> List.tryLast
+        match lastResult with
+        | Complete result ->
+            return! result.Outputs |> List.tryLast
+        | Incomplete _ ->
+            let! newResults = amplify lastResult results
+            return! continueAmplifying newResults
+    }
+
+    return! continueAmplifying results
+}
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length < 1 then
+        printfn "The input file hasn't been specified"
+    else
+        let program =
+            argv.[0]
+            |> File.ReadAllText
+            |> split ','
+            |> Array.map stringToInt
+            |> Array.choose id
+
+        [ 5..9 ]
+        |> perms
+        |> Seq.map (List.map (fun setting -> execute [ setting ] program))
+        |> Seq.map sequenceOption
+        |> Seq.choose id
+        |> Seq.map amplifyAll
+        |> Seq.choose id
+        |> Seq.max
+        |> printfn "%i"
+    0
